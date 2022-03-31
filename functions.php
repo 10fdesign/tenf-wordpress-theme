d3a2b92bf448daa4ad03f5c4fe83e38aaa287155
Oscar Heller <oheller25@gmail.com>
2022-03-31 10:48:12 -0400

    Disabled fullscreen editor

--- functions.php
+++ functions.php
@@ -92,6 +92,15 @@ endif;
 add_action( 'after_setup_theme', 'tenf_setup' );
 
 /**
+ * Disable fullscreen editor
+ */
+function tenf_disable_editor_fullscreen_mode() {
+	$script = "window.onload = function() { const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' ); if ( isFullscreenMode ) { wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' ); } }";
+	wp_add_inline_script( 'wp-blocks', $script );
+}
+add_action( 'enqueue_block_editor_assets', 'tenf_disable_editor_fullscreen_mode' );
+
+/**
  * Pretty-print function for objects.
  */
 function tenf_r( $data ) {
